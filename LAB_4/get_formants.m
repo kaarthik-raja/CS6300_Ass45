a62db841421492c85fc58e83ec5b0758b05a09d3
RedRover-dev <cs16b008@smail.iitm.ac.in>
2020-03-05 00:11:27 +0530

    Q1

---
+++ LAB_4/get_formants.m
@@ -0,0 +1,26 @@
+function[formants] = get_formants(SigTime)
+
+SigTime = SigTime(1:512,1);
+
+x = SigTime;
+x1 = x.*hamming(length(x));
+preemph = [1 0.1];
+x1 = filter(1,preemph,x1);
+
+a = lpc(x1,60);
+rts = roots(a);
+
+rts = rts(imag(rts)>=0);
+angz = atan2(imag(rts),real(rts));
+
+[frqs,indices] = sort(angz.*(Fs/(2*pi)));
+bw = -1/2*(Fs/(2*pi))*log(abs(rts(indices)));
+
+formants = zeros(1,length(frqs));
+nn = 1;
+for kk = 1:length(frqs)
+    if (frqs(kk) > 90 && bw(kk) <400)
+        formants(nn) = frqs(kk);
+        nn = nn+1;
+    end
+end
