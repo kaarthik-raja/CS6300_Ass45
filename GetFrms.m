c0ea11e1f1ab2ce4d88a2bb6ae11e5412215967f
kaarthik-raja <kaarthikrajamv@gmail.com>
2020-03-05 13:20:51 +0530

    formants

---
+++ GetFrms.m
@@ -0,0 +1,29 @@
+function formants = GetFrms(SigTime,Fs)
+
+    x = SigTime;
+    x1 = x.*hamming(length(x));
+    preemph = [1 0.1];
+    x1 = filter(1,preemph,x1);
+
+    a = lpc(x1,60);
+    rts = roots(a);
+
+    rts = rts(imag(rts)>=0);
+    angz = atan2(imag(rts),real(rts));
+
+    [frqs,indices] = sort(angz.*(Fs/(2*pi)));
+    bw = -1/2*(Fs/(2*pi))*log(abs(rts(indices)));
+
+    formants = zeros(1,length(frqs)+5);
+    nn = 1;
+    for kk = 1:length(frqs)
+        if (frqs(kk) > 90 && bw(kk) <400)
+            formants(nn) = frqs(kk);
+            nn = nn+1;
+        end
+    end
+
+end
+%%
+
+%GetFrms (SigTime,Fs)
--- Q3.m
+++ Q3.m
@@ -3,9 +3,10 @@ clear all;
 clc;
 %%
 % Global Variables Used
-audio = "sa1.F.wav";
-W_ms = 15;
-Thresh_ZC = 0.3;
+audio = "sa2.F.wav";
+W_ms = 80;
+Skip_ms = 30; %LEss than W_ms
+Thresh_ZC = 0.7;
 Thresh_SE = 0.05;
 %%
 [SigTime,Fs] = audioread(audio);
@@ -16,6 +17,10 @@ SqVal = SigTime.*SigTime;
 ZC = sign(-SigTime(1:end-1).*SigTime(2:end));
 ZC(ZC<0) = 0 ;
 
+%figure;
+%dscatter(1:len-1,ZC);
+%colorbar;
+
 Avg_ZC = sum(ZC)/len;
 Avg_SE = sum(SqVal)/len;
 
@@ -23,11 +28,12 @@ Avg_SE = sum(SqVal)/len;
 fprintf("Avg Zero Crossing = %d, Energy = %d\n",Avg_ZC,Avg_SE);
 
 W_len = int32((W_ms/1000.0)*Fs);
+Skip_len = int32((Skip_ms/1000.0)*Fs);
 ZC_Win = Avg_ZC*W_len;
 max =0;
 VAD_ZC = zeros(1,len);
 VAD_SE = zeros(1,len);
-for i = 1:50:len-W_len-5
+for i = 1:Skip_len:len-W_len-5
     TZC = sum(ZC(i:i+W_len) );
     TZC_avg = TZC/double(W_len);
     TSE = sum(SqVal(i:i+W_len) );
